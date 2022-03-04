466f83422378dc3e9aecd56ff96f411eeb799baa
jacqueli <jacqueli@student.21-school.ru>
2022-03-04 13:30:32 +0300

    now progress is gay

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "array_math.h"
 
 #define N 256
-#define ENABLE_PROGRESS 0
+#define ENABLE_PROGRESS 1
 
 void init(double *, int);
 void test_distrib(double *arr, int n, int iters, void (*func)(double *, int));
@@ -54,6 +54,13 @@ void test_distrib(double *arr, int n, int iters, void (*func)(double *, int)) {
     double *res = calloc(n, sizeof(*res));
     memset(res, n, sizeof(*res));
     while (i++ < iters) {
+        if (ENABLE_PROGRESS) {
+            printf("\033[1J-=\033[38;2;%d;%d;%dm %.3lf%% \033[0m=-\n", 
+                (int)((double)i/iters*100*4)%255,
+                (int)((double)i/iters*100*8)%255,
+                (int)((double)i/iters*100*12)%255,
+                (double)i/iters*100);
+        }
         init(arr, n);
         func(arr, n);
         array_norm(arr, arr, n);
